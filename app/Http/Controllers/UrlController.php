887e1e7dcbecd8407b13babeea273c8cebaa7f47
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-23 17:33:40 -0600

    funcion store en controlador, falta crear metodo en modelo Url

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Url;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UrlController extends Controller
 {
-    //
+
+    public function store(Request $request, Url $url)
+    {
+        $url = $url->short_url($request->long_url);
+        return response()->json([
+            'short_url' => url('/') . '/' . $url
+        ]);
+    }
 }
