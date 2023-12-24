78f2e4a9ea6c96d0dea87bf8f78de88a7d8de1a9
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-23 20:12:10 -0600

    UrlController function show

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class UrlController extends Controller
 {
+    public function show($code)
+    {
+        $url = DB::table('urls')->where('code', $code)->first();
+
+        if ($url) {
+            return redirect()->away($url->url);
+        } else {
+            abort(404);
+        }
+    }
 
     public function store(Request $request, Url $url)
     {
